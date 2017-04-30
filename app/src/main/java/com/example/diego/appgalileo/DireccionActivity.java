dd91c8e34e0e4ae75099a0ad2bc768630e091c98
diegoonia <diego.ona@live.com.ar>
2017-04-30 18:40:58 -0300

    lee y escribe archivo

--- app/src/main/java/com/example/diego/appgalileo/DireccionActivity.java
+++ app/src/main/java/com/example/diego/appgalileo/DireccionActivity.java
@@ -1,29 +1,62 @@
 package com.example.diego.appgalileo;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 public class DireccionActivity extends AppCompatActivity {
+
+    Button btnDir;
+    EditText txtPuerto, txtIp;
+    String fileName = "config.txt";
+    String dirCompleta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_direccion);
 
-
-        Button btnDir = (Button) findViewById(R.id.btnGuardar);
+        btnDir = (Button) findViewById(R.id.btnGuardar);
+        txtPuerto = (EditText) findViewById(R.id.etPuerto);
+        txtIp = (EditText) findViewById(R.id.etIp);
 
         btnDir.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                saveFile(fileName, txtIp.getText().toString() + ":" + txtPuerto.getText().toString());
                 Intent intent = new Intent(DireccionActivity.this,MainActivity.class);
 
                 startActivity(intent);
             }
         });
     }
+
+    public void saveFile(String file, String text)
+    {
+        try{
+            FileOutputStream fos = openFileOutput(file, Context.MODE_PRIVATE);
+            fos.write(text.getBytes());
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(DireccionActivity.this, "Error saving file",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
 }
--- app/src/main/java/com/example/diego/appgalileo/MainActivity.java
+++ app/src/main/java/com/example/diego/appgalileo/MainActivity.java
@@ -1,32 +1,70 @@
 
 package com.example.diego.appgalileo;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.AssetManager;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    String fileName = "config.txt";
+    Button btnDir;
+    TextView tvDir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnDir = (Button) findViewById(R.id.btnDireccion);
+        btnDir = (Button) findViewById(R.id.btnDireccion);
+        tvDir = (TextView) findViewById(R.id.tvPuertoIp);
+
+        tvDir.setText(readFile(fileName));
 
         btnDir.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        @Override
+        public void onClick(View v) {
 
-                Intent intent = new Intent(MainActivity.this,DireccionActivity.class);
+            Intent intent = new Intent(MainActivity.this,DireccionActivity.class);
 
-                startActivity(intent);
-            }
+            startActivity(intent);
+        }
         });
     }
 
+    public String readFile(String file){
+        String text = " ";
 
+        try{
+            FileInputStream fis = openFileInput(file);
+            int size = fis.available();
+            byte[] buffer = new byte[size];
+            fis.read(buffer);
+            fis.close();
+            text = new String(buffer);
+        } catch (Exception e){
+            e.printStackTrace();
+            Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+        }
+
+
+        return text;
+    }
 
 }
